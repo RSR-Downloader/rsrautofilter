22bbe1b4384f4d259cf360ac9352b26162584365
RSR-Downloader <79363075+RSR-Downloader@users.noreply.github.com>
2021-03-05 12:35:06 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -160,7 +160,7 @@ async def cb_handler(client: Bot, query:CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("HELP", callback_data="help_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")]
+                [InlineKeyboardButton("🚸 JOIN OUR CHANNEL 🚸", url="https://t.me/mizolibrary")]
             ])
 
             await query.message.edit_text(
@@ -174,7 +174,7 @@ async def cb_handler(client: Bot, query:CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="start_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")]
+                [InlineKeyboardButton("🚸 SUPPORT 🚸", url="https://t.me/mp3andvideodownloader")]
             ])
 
             await query.message.edit_text(
@@ -188,7 +188,7 @@ async def cb_handler(client: Bot, query:CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("START", callback_data="start_data")],
-                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot")]
+                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/https://github.com/RSR-Downloader/rsrautofilter")]
             ])
 
             await query.message.edit_text(
